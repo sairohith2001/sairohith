dcc7e05016dcc619799a3b8ed1126b1e318d8b9d
sairohith2001 <73826930+sairohith2001@users.noreply.github.com>
2023-12-11 11:47:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+<!-- ... (previous HTML code) ... -->
+
+<body class="bg-gray-100 font-sans">
+
+  <!-- ... (previous HTML code) ... -->
+
+  <!-- JavaScript Section -->
+  <script>
+    // JavaScript code for interactivity
+    document.addEventListener('DOMContentLoaded', function () {
+      // Get the button element by its ID
+      const interactButton = document.getElementById('interactButton');
+
+      // Add a click event listener to the button
+      interactButton.addEventListener('click', function () {
+        // Display a message when the button is clicked
+        alert('Hello! Feel free to explore my portfolio.');
+      });
+    });
+  </script>
+
+</body>
+
+</html>
