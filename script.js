145c92d52016b69aa7a43778d2c008b739827c9b
sairohith2001 <73826930+sairohith2001@users.noreply.github.com>
2023-12-11 12:05:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-<!-- ... (previous HTML code) ... -->
-
-<body class="bg-gray-100 font-sans">
-
-  <!-- ... (previous HTML code) ... -->
-
-  <!-- JavaScript Section -->
-  <script>
-    // JavaScript code for interactivity
+ // JavaScript code for interactivity
     document.addEventListener('DOMContentLoaded', function () {
       // Get the button element by its ID
-      const interactButton = document.getElementById('interactButton');
+      let interactButton = document.getElementById('interactButton');
 
       // Add a click event listener to the button
       interactButton.addEventListener('click', function () {
@@ -17,8 +9,3 @@
         alert('Hello! Feel free to explore my portfolio.');
       });
     });
-  </script>
-
-</body>
-
-</html>
